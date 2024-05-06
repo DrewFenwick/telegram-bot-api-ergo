cc5b8c331085d2d94996aa36b677f29653cdd741
DrewFenwick <drew.e.fenwick+github@gmail.com>
2024-05-06 21:12:16 +0100

    Remove the MakingRequests module for now.

--- telegram-bot-api-ergo-shim/src/Telegram/Bot/API/Ergo/MakingRequests.hs
+++
@@ -1,5 +0,0 @@
-module Telegram.Bot.API.Ergo.MakingRequests where
-import qualified Telegram.Bot.API.MakingRequests as Old
-
-mapResponse :: (a -> b) -> Old.Response a -> Old.Response b
-mapResponse f response = response {Old.responseResult = f response.responseResult} 
